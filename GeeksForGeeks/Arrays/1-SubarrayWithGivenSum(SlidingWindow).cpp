482999f2b93c90d7f1346d04c5f3b1ff1207e284
krishna-gavas <krishna.gavas@gmail.com>
2020-02-04 19:05:42 +0530

    SubarrayWithGivenSum-SlidingWindow

---
+++ GeeksForGeeks/Arrays/1-SubarrayWithGivenSum(SlidingWindow).cpp
@@ -0,0 +1,31 @@
+// Subarray with given sum using Sliding window technique
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    long int N,S,flag=0,head=0,sum=0;		// initialize head and sum
+	    cin>>N>>S;
+	    long int arr[N];
+	    for(int i=0;i<N;i++)
+	        cin>>arr[i];
+	        
+	    for(int i=0;i<N;i++){
+	        sum = sum + arr[i];          
+	        while(sum > S){						// if subarray sum is greater shift head 
+				sum = sum - arr[head];
+				head++;
+			}
+			if(sum == S){
+				cout<<head+1<<" "<<i+1<<endl;
+				flag = 1;
+				break;
+			}
+	    }
+	    if(flag==0)
+	        cout<<-1<<endl;
+	}
+	return 0;
+}
