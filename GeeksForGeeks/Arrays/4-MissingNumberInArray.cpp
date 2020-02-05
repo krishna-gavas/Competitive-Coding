2bbffcaec1a86c6c6950716d2532ec5eeb2bab82
krishna-gavas <krishna.gavas@gmail.com>
2020-02-05 14:00:19 +0530

    MissingNumberInArray

---
+++ GeeksForGeeks/Arrays/4-MissingNumberInArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    long int N,sum=0,num,total;
+	    cin>>N;
+		total = (N*(N+1))/2;
+		cout<<total<<endl;
+	    long int arr[N];
+	    for(long int i=0;i<N-1;i++){
+	        cin>>arr[i];
+			sum = sum + arr[i];
+		}
+
+		cout<<total-sum<<endl;
+		
+	}
+	return 0;
+}
