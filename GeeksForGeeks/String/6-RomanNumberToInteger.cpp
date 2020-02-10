1e72dba69c4415d0e249dd82e1759aed644b54f8
krishna-gavas <krishna.gavas@gmail.com>
2020-02-10 15:49:27 +0530

    RomanNumberToInteger

---
+++ GeeksForGeeks/String/6-RomanNumberToInteger.cpp
@@ -0,0 +1,44 @@
+// Roman Number to Integer
+
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+    map<char, int> roman;
+    roman.insert(pair<char, int>('I', 1));
+    roman.insert(pair<char, int>('V', 5));
+    roman.insert(pair<char, int>('X', 10));
+    roman.insert(pair<char, int>('L', 50));
+    roman.insert(pair<char, int>('C', 100));
+    roman.insert(pair<char, int>('D', 500));
+    roman.insert(pair<char, int>('M', 1000));
+
+	while(T--){
+	    string str1;
+        cin>>str1;
+        int len = str1.length(),num,sum=0;
+
+        for(int i=0;i<len;){
+            if(i == (len-1)){
+                num = roman[str1[i]];
+                i++;
+            }
+            else if(roman[str1[i]] < roman[str1[i+1]]){
+                num = roman[str1[i+1]] - roman[str1[i]];
+                i = i+2;
+            }
+            else{
+                num = roman[str1[i]];
+                i++;
+            }
+            sum = sum + num;
+        }
+
+        cout<<sum<<endl;
+	}
+	return 0;
+}
