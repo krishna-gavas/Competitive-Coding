8dfc145961c42cab7edef8bcc1f9d784a6804fc8
krishna-gavas <krishna.gavas@gmail.com>
2020-02-15 14:40:06 +0530

    codechef nochange

--- Codechef/longcook.cpp
+++ Codechef/longcook.cpp
@@ -7,92 +7,94 @@ int main() {
 	long int T;
 	cin>>T;
 	while(T--){
-	    long int m1,m2,month,lcs,lce,co,day,d=1,d1,count=0;
-        long int y1,y2,y,year,year1;
+	    long int m1,m2,day,d=1,count=0,flag=0;
+        long int y1,y2,y,year;
         cin>>m1>>y1;
         cin>>m2>>y2;
+        if(m1 > 2)
+            y1 = y1 + 1;
 
-        if(y1==y2 && m1>m2){
-            cout<<-1<<endl;
-            return 0;
-        }
-        if(m1 > 2){
-            month = 2;
-            year = y1 + 1;
+        if(m2 == 1)
+            y2 = y2 - 1;
+
+        int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 }; 
+        int start,start1,start2,end,end1,end2;
+        start1 = y1;end2=y2;
+        if(y1 % 400 == 0){
+            end1 = y1;
+            end = y1;
         }
         else{
-            month = 2;
-            year = y1;
-        }
-        int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };  
-
-        while(year < y2){
-            //code
-            int feb = 28;
-            if((year%4 == 0 && year%100 != 0) || (year%400 == 0))
-                feb = 29;
-            y = year;
-            y -= month < 3;
-            day = ( y + y / 4 - y / 100 +  y / 400 + t[month - 1] + d) % 7;
-            day = (day - 5)%7;
-            if(day >= 0)
-                day = (7 - day)%7;
-            else 
-                day = 0 - day;
-            lcs = day + d;
-            lce = lcs + 10;
-
-            if(lcs > 5)
-                d1 = lcs - 5;
-            else
-                d1 = lcs + 2;
-            int i;
-            for(i=d1;i<=feb;i=i+7)
-                co = i;
-            co = co - 7;
-            if(co >= lcs && co<= lce){
-                count++;
-                cout<<year<<" "<<month<<endl;
-                cout<<lcs<<" "<<lce<<" "<<co<<endl;
+            end = ((y1/400)+1)*400;
+            end1 = y2;
+            if(y2-y1 >=400){
+                flag = 1;
+                if(y1 % 400 <= 100){
+                    count = count + 76;
+                    end1 = (y1/400)*400 + 100;
+                } 
+                else if(y1 % 400 <= 200){
+                    count = count + 52;
+                    end1 = (y1/400)*400 + 200;
+                }
+                else if(y1 % 400 <= 300){
+                    count = count + 26;
+                    end1 = (y1/400)*400 + 300;
+                }
             }
+        }
 
+        if(y2 % 400 == 0){
+            start2 = y2;
+            start = y2;
+        }
+        else {
+            start = ((y2/400)*400);
+            start2 = y2;
+            if(y2-y1 >=400){
+                if(y2 % 400 >= 300){
+                    start2 = (y2/400)*400 ;
+                }
+                else if(y2 % 400 >= 200){
+                    start2 = (y2/400)*400 ;
+                }
+                else if(y2 % 400 >= 100){
+                    start2 = (y2/400)*400 ;
+                }
+            }
+        }
+
+        year = start1;
+        while(year < end1){
+            //code
+            y = year;
+            y -= 2 < 3;
+            day = ( y + (y/4) - (y/100) +  (y/400) + t[1] + d) % 7;
+            
+            if(day == 0 && ((year%4 == 0 && year%100 != 0) || (year%400 == 0)) == false)
+                count++;
+ 
+            else if(day == 6)
+                count++;
             year++;           
         }
 
-        while(year == y2 && month <= m2){
+        year = start2;
+        while(year <= end2){
             //code
-            int feb = 28;
-            if((year%4 == 0 && year%100 != 0) || (year%400 == 0))
-                feb = 29;
             y = year;
-            y -= month < 3;
-            day = ( y + y / 4 - y / 100 +  y / 400 + t[month - 1] + d) % 7;
-            day = (day - 5)%7;
-            if(day > 0)
-                day = 7 - day;
-            else 
-                day = 0 - day;
-            lcs = day + d;
-            lce = lcs + 10;
-
-            if(lcs > 5)
-                d1 = lcs - 5;
-            else
-                d1 = lcs + 2;
-            int i;
-            for(i=d1;i<=feb;i=i+7)
-                co = i;
-            co = co - 7;
-            if(co >= lcs && co<= lce){
+            y -= 2 < 3;
+            day = ( y + (y/4) - (y/100) +  (y/400) + t[1] + d) % 7;
+            
+            if(day == 0 && ((year%4 == 0 && year%100 != 0) || (year%400 == 0)) == false)
                 count++;
-                cout<<year<<" "<<month<<endl;
-                cout<<lcs<<" "<<lce<<" "<<co<<endl;
-            }
-
-            if(month == 0)
-                year++;
-            month = (month + 1) % 12;           
+ 
+            else if(day == 6)
+                count++;
+            year++;           
         }
+        if(flag == 1)
+            count = count + ((start-end)/400)*101;
 
         cout<<count<<"\n";
 	}
--- Codechef/nochange.cpp
+++ Codechef/nochange.cpp
@@ -36,8 +36,34 @@ int main() {
                 cout<<0<<" ";
             cout<<endl;
         }
-        else
-            cout<<"NO"<<endl;
+        else{
+            int flag2 = 0,sum=0;
+            for(int i=N-1;i>0;i--){
+                if((D[i] % D[i-1]) != 0){
+                    flag2 = 1;
+                    sum = sum + ((P/D[i])-1)*D[i];
+                    C[i] = (P/D[i])-1;
+                    C[i-1] = 0;
+                    while(sum < P){
+                        sum = sum + D[i-1];
+                        C[i-1] = C[i-1] + 1;
+                    }
+                    pos = i-1;
+                    break;
+                }
+                C[i] = 0;
+            }
+            if(flag2 == 0)
+                cout<<"NO"<<endl;
+            else{
+                cout<<"YES ";
+                for(int i=0;i<pos;i++)
+                    cout<<0<<" ";
+                for(int i=pos;i<N;i++)
+                    cout<<C[i]<<" ";
+                cout<<endl;
+            }
+        }
 	}
 	return 0;
 }
