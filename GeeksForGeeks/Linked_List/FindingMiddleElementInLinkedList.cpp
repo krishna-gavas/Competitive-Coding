11761ceda81da216ccd456fe663d2b57d4d279e9
krishna-gavas <krishna.gavas@gmail.com>
2020-03-03 20:52:15 +0530

    FindingMiddleElementInLinkedList

--- GeeksForGeeks/Linked_List/FindingMiddleElementInLinkedList.cpp
+++ GeeksForGeeks/Linked_List/FindingMiddleElementInLinkedList.cpp
@@ -35,19 +35,37 @@ void Insert(struct Node** head, int newdata){
     }
 }
 
+int getMiddle(struct Node** head){
+    struct Node* temp = *head;
+    struct Node* mid = *head;
+    int count=0;
+    while(temp != NULL){
+        temp = temp->next;
+        count++;
+    }
+    count = (count/2) + 1;
+    while(count > 1){
+        mid = mid->next;
+        count--;
+    }
+    return mid->data;
+}
+
 int main() {
 	int T;
 	cin>>T;
 	while(T--){
 	    struct Node* head = NULL;
-        int N,l;
+        int N,l,out;
 
         cin>>N;
         for(int i=0;i<N;i++){
             cin>>l;
             Insert(&head,l);
         }
-        Print(&head);
+        // Print(&head);
+        out = getMiddle(&head);
+        cout<<out<<endl;
 	}
 	return 0;
 
