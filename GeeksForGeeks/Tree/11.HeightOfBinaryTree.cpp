e3dc6cb5aa68f0ee2ebd4a4dcf455cfcf637745f
krishna-gavas <krishna.gavas@gmail.com>
2020-05-14 09:35:25 +0530

    HeightOfBinaryTree

---
+++ GeeksForGeeks/Tree/11.HeightOfBinaryTree.cpp
@@ -0,0 +1,122 @@
+// Given a binary tree, check whether it is a mirror of itself
+
+// Accept a string s and call buildTree(s) function.
+// In buildTree() copy the string elements into a vector.
+//     build a queue and push root into it.
+//     Access all the vector elements and also check if the queue is not empty:
+//     initialize i=1(first element access)
+//     For every access make currNode = queue.front() and currVal = ip[i]
+//     check if(currVal != "N") and then make currNode->left as currVal and push it to queue and increment i.
+//     make currVal = ip[i], check if(currVal != "N") and then make currNode->right as currVal and push it to queue and increment i.
+//     finally return root.
+// call height(root) function: if root is NULL return 0.
+//     find leftHeight = height(root->left) and rightHeight = height(root->right)
+//     return maximum(leftHeight,rightHeight) + 1.
+
+
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <sstream>
+using namespace std;
+
+struct Node
+{
+   int data;
+   Node* left;
+   Node* right;
+};
+
+Node* newNode(int val)
+{
+   Node* temp = new Node;
+   temp->data = val;
+   temp->left = NULL;
+   temp->right = NULL;
+
+   return temp;
+}
+
+bool height(struct Node* root);
+
+Node* buildTree(string str)
+{
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   Node* root = newNode(stoi(ip[0]));
+
+   queue<Node*> queue;
+   queue.push(root);
+
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       Node* currNode = queue.front();
+       queue.pop();
+
+       string currVal = ip[i];
+
+       if(currVal != "N") {
+
+           currNode->left = newNode(stoi(currVal));
+           queue.push(currNode->left);
+       }
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       if(currVal != "N") {
+
+           currNode->right = newNode(stoi(currVal));
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+int main() {
+ 
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        cout<<height(root)<<endl;
+   }
+   return 0;
+}
+
+int maximum(int a, int b){
+    if(a > b)
+        return a;
+    else 
+        return b;
+}
+
+bool height(struct Node* root){
+    if(root==NULL)
+        return 0;
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    int ret =  maximum(leftHeight,rightHeight);
+    return ret+1;
+}
