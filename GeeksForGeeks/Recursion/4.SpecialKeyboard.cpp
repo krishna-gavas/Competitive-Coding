37a71982bccab84a54a0eb32c22c3e64b75c0fe4
krishna-gavas <krishna.gavas@gmail.com>
2020-05-26 22:36:12 +0530

    SpecialKeyboard

---
+++ GeeksForGeeks/Recursion/4.SpecialKeyboard.cpp
@@ -0,0 +1,24 @@
+// Special Keyboard
+
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int keyboard(int n){
+    if(n < 5)
+        return n;
+    return max(4*keyboard(n-5), 3*keyboard(n-4));
+}
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int N,S;
+        cin>>N;
+        cout<<keyboard(N)<<endl;
+    }
+    return 0;
+}
