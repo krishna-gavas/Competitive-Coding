c15920daeaa1bb671eec5b197790f51052dcd486
krishna-gavas <krishna.gavas@gmail.com>
2020-06-10 11:48:52 +0530

    string/LongestPalindromeInString

--- GeeksForGeeks/String/3-LongestPalindromeInString.cpp
+++ GeeksForGeeks/String/3-LongestPalindromeInString.cpp
@@ -1,65 +1,79 @@
-// Longest Palindrome in a String( Abort signal from abort(3) (SIGABRT) )
+// Longest Palindrome in a String(Manacher's Algorithm)
+
+// Create an array T to store string aba as $#a#b#a#@ and create another array P to store palindrome count at every character of T.
+// Manacher's Algorithm: initialize Centre C=1, Right boundary R = 1 and maxlen = 0.
+//     for(i=1 to n-1): mirror_of_i = 2*C -i.
+//         if(i < R(i.e i lies within the boundary)) then P[i] = min(R-i, P[mirr])
+//         while(T[i + (1+P[i])] == T[i - (1+P[i])]) increment P[i].
+//         if(P[i] > maxlen) then maxlen = P[i].
+//         if(i + P[i] > R) then update the centre C = i and also update right boundary R = i + P[i].
+//     for(i=0 to n)(this is required to get the first occurence):
+//         if(P[i] == maxlen):
+//             if(i is odd) then l = (i-2)/2 and l = l - (maxlen/2 - 1) else l = (i-2)/2 - maxlen/2.
+//             break from the loop.
+//     print substring of str from l upto maxlen count.
+
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main() {
 	int T;
 	cin>>T;
 	while(T--){
-	    string str1;
-        cin>>str1;
-        int len = str1.length(),start,begin1,begin2;
-        int l,r,count,max1=0,max2=0;
+	    string str;
+        cin>>str;
+        int len = str.length();
+        if(len < 2){
+            cout<<str<<endl;
+            continue;
+        }
+        vector<char> T;
+        T.push_back('$');
+        T.push_back('#');
+        int n=2,C=1,R=1,maxlen = 0;
+        for(int i=0;i<len;i++){
+            T.push_back(str[i]);
+            T.push_back('#');
+            n += 2;
+        }
+        T.push_back('@');
+        n++;
+        int P[n];
+        for(int i=0;i<n;i++)
+            P[i] = 0;
 
-        if(len < 2)
-            cout<<str1<<endl;
+        for(int i=1;i<n-1;i++){
+            int mirr = 2*C -i;
+            if(i < R)
+                P[i] = min(R-i, P[mirr]);
+            
+            while(T[i + (1+P[i])] == T[i - (1+P[i])])
+                P[i]++;
 
-        // //-------------Odd length palindrome----------------
-		for(int i=1;i<len-1;i++){
-            l = i-1;
-            r = i+1;
-            count = 1;
-            while((l>=0) && (r<len) && (str1[l]==str1[r])){
-                count = count + 2;
-                start = l;
-                l--;
-                r++;
-            }
-            if(count > max1){
-                max1 = count;
-                begin1 = start;
-            }
-        }          
-
-        // -------------even length palindrome----------------
-        int j;
-        for(int i=0;i<len-1;i++){   
-            j = i+1;
-            count = 0;
-            if(str1[i]==str1[j]){
-                l = i-1;
-                r = j+1;
-                count = count + 2;
-                start = l;
-                while((l>=0) && (r<len) && (str1[l]==str1[r])){
-                    count = count + 2;
-                    start = l;
-                    l--;
-                    r++;
-                }
-                if(count > max2){
-                    max2 = count;
-                    begin2 = start;
-                }
+            if(P[i] > maxlen)
+                maxlen = P[i];
+            if(i + P[i] > R){
+                C = i;
+                R = i + P[i];
             }
         }
-
-        if(max1 > max2)
-            cout<<str1.substr(begin1,max1)<<endl;
-        else
-            cout<<str1.substr(begin2,max2)<<endl;  
+        int l;
+        for(int i=0;i<n;i++){
+            if(P[i] == maxlen){
+                if(i%2 == 1){
+                    l = (i-2)/2;
+                    l = l - (maxlen/2 - 1);
+                }
+                else{
+                    l = (i-2)/2 - maxlen/2;
+                }
+                break;
+            }
+        }
+        cout<<str.substr(l,maxlen)<<endl;
 	}
 	return 0;
 }
