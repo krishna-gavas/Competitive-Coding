69e77f431f1f0a9387eb6eefe4bea018cf253a30
krishna-gavas <krishna.gavas@gmail.com>
2020-06-10 08:20:39 +0530

    TopologicalSort

--- GeeksForGeeks/Graph/5.TopologicalSort.cpp
+++ GeeksForGeeks/Graph/5.TopologicalSort.cpp
@@ -1,11 +1,12 @@
-// Detect cycle in an directed graph(using Topological sort)
+// Topological sort
 
-// isCyclic(int V, vector<int> g[]): create an array to store indegree of all nodes and initialize count to 0.
+// topoSort(int V, vector<int> g[]): Create a result array using dynamic memory allocation(since C/C++ cannot return pointer to a local variable).
+//     create an array to store indegree of all nodes and initialize count to 0.
 //     Create a queue and push all the nodes to it whose inDegree is 0.
-//     while(q is not empty): pop the front of q and store it in start, push start to result vector.
+//     while(q is not empty): pop the front of q and store it in start, res[k++] = start.
 //         for(it = g[start].begin() to g[start].end()): increment indegree[*it] and if(indegree[*it] == 0) then push *it to q.
 //         increment count.
-//     if(count != V) return true else return false.
+//     return res.
 
 #include <iostream>
 #include <queue>
@@ -13,12 +14,23 @@
 #include <algorithm>
 using namespace std;
 
-bool isCyclic(int V, vector<int> g[])
+bool check(int V,int res[], vector<int> g[]){
+    vector<int> map(V, -1);
+    for(int i=0;i<V;i++)
+        map[res[i]]++;
+    for(int i=0;i<V;i++)
+        for(int v: g[i])
+            if(map[i] > map[v])
+                return false;
+    return true;
+}
+
+int* topoSort(int V, vector<int> g[])
 {
-    vector<int> res;
+    int *res = new int[V];
     vector<int> :: iterator it;
     queue<int> q;
-    int indegree[V],count=0,start;
+    int indegree[V],count=0,start,k=0;
     for (int i = 0; i < V; i++)
         indegree[i] = 0;
     for (int i = 0; i < V; i++)
@@ -32,7 +44,7 @@ bool isCyclic(int V, vector<int> g[])
     while(!q.empty()){
         start = q.front();
         q.pop();
-        res.push_back(start);
+        res[k++] = start;
         for(it = g[start].begin(); it != g[start].end(); it++){
             indegree[*it]--;
             if(indegree[*it] == 0)
@@ -40,10 +52,7 @@ bool isCyclic(int V, vector<int> g[])
         }
         count++;
     }
-    if(count != V)
-        return true;
-    else 
-        return false;
+    return res;
 }
 
 int main(){
@@ -60,7 +69,9 @@ int main(){
             adj[u].push_back(v);
         }
 
-        cout<<isCyclic(V, adj)<<endl;
+        int *res = topoSort(V, adj);
+        cout<<check(V, res, adj)<<endl;
+        
     }
     return 0;
 }
