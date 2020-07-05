733644a2c70e34f6c4e82a5e105607c6be89e627
krishna-gavas <krishna.gavas@gmail.com>
2020-07-05 17:40:35 +0530

    SearchInARotatedArray

--- GeeksForGeeks/DivideAndConquer/1.FindElementThatAppearsOnceInSortedArray.cpp
+++ GeeksForGeeks/DivideAndConquer/1.FindElementThatAppearsOnceInSortedArray.cpp
@@ -1,16 +1,13 @@
 // Find the element that appears once in sorted array
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
 using namespace std;
 
 int main(){
     int T;
     cin>>T;
     while(T--){
-        int N,mid,left=0,right=N-1;
+        int N;
         cin>>N;
         long int A[N];
         for(int i=0;i<N;i++)
