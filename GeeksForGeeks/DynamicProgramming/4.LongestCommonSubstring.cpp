cc99a3ee5b45c78573d7d75d9849131c39d965a8
krishna-gavas <krishna.gavas@gmail.com>
2020-08-31 20:32:43 +0530

    Longest common substring

---
+++ GeeksForGeeks/DynamicProgramming/4.LongestCommonSubstring.cpp
@@ -0,0 +1,46 @@
+// Longest Common Substring
+
+// Create a DP table and initialize the length of longest substring(len) as 0
+// for(i=0 to m+1)
+//     for(j=0 to n+1)
+//         if(it is first row or first column) then DP[i][j] = 0
+//         else if(left and top values are equal) then DP[i][j] = DP[i-1][j-1] + 1
+//         else then DP[i][j] = 0
+//         if(DP[i][j] > len) then len = DP[i][j]
+// return len
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int LCS(string X, string Y, int m, int n){
+    int DP[m+1][n+1];
+    int len=0;
+    for(int i=0;i<=m;i++){
+        for(int j=0;j<=n;j++){
+            if(i==0 || j==0)
+                DP[i][j] = 0;
+            else if(X[i-1] == Y[j-1])
+                DP[i][j] = DP[i-1][j-1] + 1;
+            else 
+                DP[i][j] = 0;
+            if(DP[i][j] > len)
+                len = DP[i][j];
+        }
+    }
+    return len;
+}
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int N,M;
+        cin>>N>>M;
+        string X,Y;
+        cin>>X>>Y;
+        cout<<LCS(X,Y,N,M)<<"\n";
+	}
+	return 0;
+}
--- codes/Sorting/QuickSort.cpp
+++ codes/Sorting/QuickSort.cpp
@@ -2,56 +2,40 @@
 #include <algorithm>
 using namespace std;
 
-// Using last element as pivot
-// int Partition(int A[], int l, int r){  
-//     int pivot = A[r];   
-//     int i = (l - 1);  
-  
-//     for (int j = l; j <= r - 1; j++){    
-//         if (A[j] < pivot)  {  
-//             i++;  
-//             swap(A[i],A[j]);  
-//         }  
-//     }  
-//     swap(A[i + 1], A[r]);  
-//     return (i + 1);  
-// }
+int Partition(int A[], int L, int R){
+    int pivot = A[L];
+    int i = R+1;
 
-// Using first element as pivot
-int Partition(int A[],int l,int r){
-    int pivot =A[l];   
-    int i = (r + 1);  
-  
-    for (int j = r; j > l; j--){   
-        if (A[j] > pivot){  
-            i--; 
-            swap(A[i], A[j]);  
-        }  
-    }  
-    swap(A[i - 1], A[l]);  
-    return (i - 1);
+    for(int j=R; j>L; j--){
+        if(A[j] > pivot){
+            i--;
+            swap(A[i], A[j]);
+        }
+    }
+    swap(A[i-1], A[L]);
+    return i-1;
 }
 
-void QuickSort(int A[],int l,int r){
-    int q;
-    if(l < r){
-        q = Partition(A,l,r);
-        QuickSort(A,l,q-1);
-        QuickSort(A,q+1,r);
+void QuickSort(int A[], int L, int R){
+    if(L < R){
+        int Q = Partition(A,L,R);
+        QuickSort(A,L,Q-1);
+        QuickSort(A,Q+1,R);
     }
 }
 
 int main() {
     int N;
-	cin>>N;
+    cin>>N;
     int arr[N];
-	for(int i=0;i<N;i++)
+    for(int i=0;i<N;i++)
         cin>>arr[i];
 
-	QuickSort(arr,0,N-1);
+    QuickSort(arr,0,N-1);
 
     for(int i=0;i<N;i++)
         cout<<arr[i]<<" ";
-    cout<<endl;
-	return 0;
+    cout<<"\n";
+
+    return 0;
 }
